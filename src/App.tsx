575b0ff6e2e8d643ba94cd481c253cdeecdbe6c6
Noctsol <39346808+Noctsol@users.noreply.github.com>
2022-09-07 23:08:54 -0700

    Updating

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,69 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Different data types
+let myvar= "yolo123";
+let age: number;
+let isStuden:boolean;
+let hobbies:string[];
+let ages: number[];
+let role: [number, string];
+let ArrayOfTuples: [number, string][];
+let objThatisStringAndInt: number | string
+
+let unknType: unknown;
+
+age = 5;
+
+type Person= {
+  name: string;
+  age?: number;
+  something?: 4
+
+}
+
+let somePerson: Person = {
+  name: "someguy"
+}
+
+let lotOfPersons: Person[];
+
+
+// Function Examples
+// let print: (msg:string) => void;
+function print(msg:string) {
+  console.log(msg);
+}
+print("hello")
+
+interface IThing {
+  prop1:string
+}
+
+type Thing = IThing & {
+  yo: string
+}
+
+
+type combinedA = {
+  A: number;
+}
+type combinedB = combinedA &{
+  B: string
+}
+
+let cB: combinedB = {
+    A: 2,
+    B: "hello"
+}
+
+
 function App() {
   return (
     <div className="App">
+      <div>
+        hello
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
